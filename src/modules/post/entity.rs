323b714596efdc0a4679cddf84100cbedd759ba5
Dave136 <davejs136@gmail.com>
2022-05-05 13:12:55 -0400

    add: scope enum

--- src/modules/post/entity.rs
+++ src/modules/post/entity.rs
@@ -1,16 +1,32 @@
-use async_graphql::SimpleObject;
+use async_graphql::{Enum, SimpleObject};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::modules::user::User;
 
+#[derive(Copy, Clone, Debug, Deserialize, Enum, PartialEq, Eq, Serialize, sqlx::Type)]
+#[sqlx(rename_all = "lowercase")]
+pub enum Scope {
+    Public,
+    Private,
+}
+
+impl ToString for Scope {
+    fn to_string(&self) -> String {
+        match &self {
+            Self::Public => String::from("public"),
+            Self::Private => String::from("private"),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize, SimpleObject)]
 pub struct Post {
     pub id: Uuid,
     pub content: String,
     pub author: User,
-    pub scope: String,
+    pub scope: Scope,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
 }
